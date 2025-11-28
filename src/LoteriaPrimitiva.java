ef2f48e8b37482059b3cbf39cdde5a1ee0e9661e
pablo <pablo.hergue@educa.jcyl.es>
2025-11-28 10:21:37 +0100

    commit1

--- src/LoteriaPrimitiva.java
+++ src/LoteriaPrimitiva.java
@@ -1,13 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Arrays;
+import java.util.Random;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+/* Partiendo del ejercicio de Loteria Primitiva que genera una combinacion de 6 numeros ordenada.
+Crea una función que reciba dos arrays, uno con los 6 números de la puesta y otro
+con los 6 números de la combinacion ganadora (ordenada). La funcion devolvera el número de aciertos.
+Arrays.binarySearch */
+
+public class LoteriaPrimitiva {
+    public static void main(String[] args) {
+        int[] apuesta = generarNumerosAleatorios();
+        int[] ganadora = generarNumerosAleatorios();
+
+
+        int aciertos = contarAciertos(apuesta, ganadora);
+
+        System.out.println("Apuesta: " + Arrays.toString(apuesta));
+
+        System.out.println("Combinación ganadora: " + Arrays.toString(ganadora));
+
+        System.out.println("Aciertos: " + aciertos);
     }
+
+    public static int[] generarNumerosAleatorios() {
+        Random random = new Random();
+        int[] apuesta = new int[6];
+        int i = 0;
+
+        while (i < 6) {
+            int n = random.nextInt(50);
+            boolean repetido = false;
+
+            for (int j = 0; j < i; j++) {
+                if (apuesta[j] == n) {
+                repetido = true;
+                break;
+
+            }
+        }
+        if (!repetido) {
+            apuesta[i] = n;
+            i++;
+        }
+    }
+    return apuesta;
+}
+
+  public static int contarAciertos(int[] apuesta, int[] ganadora) {
+        int aciertos=0;
+
+        for(int numero : apuesta) {
+
+            if (Arrays.binarySearch(ganadora, numero) >= 0) {
+                aciertos++;
+            }
+
+        }
+        return aciertos;
+
+  }
+
 }
